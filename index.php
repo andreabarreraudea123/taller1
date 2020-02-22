ecbeb255ecc89c107a79375d1d23f133dbddc1dc
andreabarreraudea123 <61071219+andreabarreraudea123@users.noreply.github.com>
2020-02-21 21:10:03 -0500

    ejercicio1

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
 
-echo("hola mundo");
+echo "TALLER 1 GENERALIDADES PHP. Elaborado por Andrea Barrera Olivares";
+
+echo "<br> <br> Ejercicio 1 <br><br>" ;
+$numero1 =20;
+$numero2 =16;
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+$multiplicacion=$numero1*$numero2;
+$division=$numero1/$numero2;
+
+echo ("La suma es: ".$numero1."+".$numero2."=".$suma);
+print("<br>");
+echo ("La resta es: ".$numero1."-".$numero2."=".$resta);
+print("<br>");
+echo ("La multiplicacion es: ".$numero1."*".$numero2."=".$multiplicacion);
+print("<br>");
+echo ("La division es: ".$numero1."/".$numero2."=".$division);
+
+
+
 
 
 ?>
